5fc2ae8c6ea05945b5896d11d9291c211b32c225
NhatKhai352528 <nhatkhai352528@gmail.com>
2023-09-22 22:18:38 +0700

    init Exercise6

---
+++ STM32/Core/Inc/clock_function.h
@@ -0,0 +1,18 @@
+/*
+ * clock_function.h
+ *
+ *  Created on: Sep 22, 2023
+ *      Author: Nhat Khai
+ */
+
+#ifndef INC_CLOCK_FUNCTION_H_
+#define INC_CLOCK_FUNCTION_H_
+
+#include "main.h"
+#include "software_timer.h"
+
+#define LED_NUMBER 12
+
+void testClock(int duration);
+
+#endif /* INC_CLOCK_FUNCTION_H_ */
